b536d57c1a881a0f5191d1052f579a8db1b4feb7
hbenziat1318-cloud <h.benziat1318@uca.ac.ma>
2025-11-04 00:40:40 +0100

    Add files via upload

---
+++ spring/src/main/java/ma/rest/spring/entities/Compte.java
@@ -0,0 +1,26 @@
+﻿package ma.rest.spring.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Compte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double solde;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+
+    @Enumerated(EnumType.STRING)
+    private TypeCompte type;
+}
---
+++ spring/src/main/java/ma/rest/spring/entities/TypeCompte.java
@@ -0,0 +1,5 @@
+﻿package ma.rest.spring.entities;
+
+public enum TypeCompte {
+    COURANT, EPARGNE
+}
